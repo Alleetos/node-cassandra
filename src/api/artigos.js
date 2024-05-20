a1cfe15954f4e5b17a3600d058a69d61cf78bc43
Alison Santos <ti6@qualitashumanus.com.br>
2024-05-20 09:52:32 -0300

    ultimas rotas

--- src/api/artigos.js
+++ src/api/artigos.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
-const client = require('../database/database');
+const { v4: uuidv4 } = require("uuid");
+const client = require("../database/database");
 
 // Rota para pegar todos os artigos
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Consulta CQL para selecionar todos os artigos
-    const query = 'SELECT * FROM facens.artigos';
+    const query = "SELECT * FROM facens.artigos";
 
     // Executa a consulta
     const result = await client.execute(query);
@@ -15,22 +15,47 @@ router.get('/', async (req, res) => {
     // Retorna os artigos como resposta
     res.json(result.rows);
   } catch (error) {
-    console.error('Erro:', error);
-    res.status(500).json({ error: 'Erro ao buscar artigos' });
+    console.error("Erro:", error);
+    res.status(500).json({ error: "Erro ao buscar artigos" });
   }
 });
 
-// Rota para salvar um novo artigo
-router.post('/', async (req, res) => {
+// Rota para buscar todos os artigos em uma categoria específica
+router.get("/categoria/:categoria", async (req, res) => {
   try {
-    const { autor_id, titulo, conteudo, tags, visualizacoes } = req.body;
+    const category = req.params.categoria;
+
+    // Consulta CQL para selecionar os artigos na categoria específica
+    const query =
+      "SELECT * FROM facens.artigos WHERE categoria = ? ALLOW FILTERING";
+
+    // Executa a consulta passando a categoria como parâmetro
+    const result = await client.execute(query, [category]);
+
+    // Retorna os artigos encontrados como resposta
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Erro:", error);
+    res.status(500).json({ error: "Erro ao buscar artigos na categoria" });
+  }
+});
+
+module.exports = router;
+
+// Rota para salvar um novo artigo
+router.post("/", async (req, res) => {
+  try {
+    const { autor_id, titulo, categoria, conteudo, tags, visualizacoes } =
+      req.body;
 
     // Verificar se o autor existe
-    const autorQuery = 'SELECT * FROM facens.autores WHERE autor_id = ?';
-    const autorResult = await client.execute(autorQuery, [autor_id], { prepare: true });
+    const autorQuery = "SELECT * FROM facens.autores WHERE autor_id = ?";
+    const autorResult = await client.execute(autorQuery, [autor_id], {
+      prepare: true,
+    });
 
     if (autorResult.rowLength === 0) {
-      return res.status(404).json({ error: 'Autor não encontrado' });
+      return res.status(404).json({ error: "Autor não encontrado" });
     }
 
     // Gera um novo UUID para o artigo
@@ -40,70 +65,81 @@ router.post('/', async (req, res) => {
     const data_publicacao = new Date();
 
     // Consulta CQL para inserir um novo artigo
-    const artigoQuery = 'INSERT INTO facens.artigos (autor_id, id, titulo, conteudo, data_publicacao, tags, visualizacoes) VALUES (?, ?, ?, ?, ?, ?, ?)';
-    const params = [autor_id, id, titulo, conteudo, data_publicacao, tags, visualizacoes];
+    const artigoQuery =
+      "INSERT INTO facens.artigos (autor_id, id, titulo, categoria, conteudo, data_publicacao, tags, visualizacoes) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    const params = [
+      autor_id,
+      id,
+      titulo,
+      categoria,
+      conteudo,
+      data_publicacao,
+      tags,
+      visualizacoes,
+    ];
 
     // Executa a consulta
     await client.execute(artigoQuery, params, { prepare: true });
 
     // Retorna uma mensagem de sucesso
-    res.status(201).json({ message: 'Artigo salvo com sucesso!' });
+    res.status(201).json({ message: "Artigo salvo com sucesso!" });
   } catch (error) {
-    console.error('Erro:', error);
-    res.status(500).json({ error: 'Erro ao salvar artigo' });
+    console.error("Erro:", error);
+    res.status(500).json({ error: "Erro ao salvar artigo" });
   }
 });
 
 // Rota para atualizar um artigo existente
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const id = req.params.id;
 
     // Verificar se o artigo existe
-    const checkQuery = 'SELECT id FROM facens.artigos WHERE id = ?';
-    const checkResult = await client.execute(checkQuery, [id], { prepare: true });
+    const checkQuery = "SELECT id FROM facens.artigos WHERE id = ?";
+    const checkResult = await client.execute(checkQuery, [id], {
+      prepare: true,
+    });
 
     if (checkResult.rowLength === 0) {
-      return res.status(404).json({ error: 'Artigo não encontrado' });
+      return res.status(404).json({ error: "Artigo não encontrado" });
     }
 
     // Parâmetros para a atualização
-    const { titulo, conteudo, tags } = req.body;
+    const { titulo, categoria, conteudo, tags } = req.body;
 
     // Consulta CQL para atualizar o artigo
-    const updateQuery = 'UPDATE facens.artigos SET titulo = ?, conteudo = ?, tags = ? WHERE id = ?';
-    const params = [titulo, conteudo, tags, id];
+    const updateQuery =
+      "UPDATE facens.artigos SET titulo = ?, categoria = ? conteudo = ?, tags = ? WHERE id = ?";
+    const params = [titulo, categoria, conteudo, tags, id];
 
     // Executa a consulta de atualização
     await client.execute(updateQuery, params, { prepare: true });
 
     // Retorna uma mensagem de sucesso
-    res.json({ message: 'Artigo atualizado com sucesso!' });
+    res.json({ message: "Artigo atualizado com sucesso!" });
   } catch (error) {
-    console.error('Erro:', error);
-    res.status(500).json({ error: 'Erro ao atualizar artigo' });
+    console.error("Erro:", error);
+    res.status(500).json({ error: "Erro ao atualizar artigo" });
   }
 });
 
-
-
 // Rota para excluir um artigo existente
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id;
 
     // Consulta CQL para excluir o artigo
-    const deleteQuery = 'DELETE FROM facens.artigos WHERE id = ?';
+    const deleteQuery = "DELETE FROM facens.artigos WHERE id = ?";
     const params = [id];
 
     // Executa a consulta de exclusão
     await client.execute(deleteQuery, params, { prepare: true });
 
     // Retorna uma mensagem de sucesso
-    res.json({ message: 'Artigo excluído com sucesso!' });
+    res.json({ message: "Artigo excluído com sucesso!" });
   } catch (error) {
-    console.error('Erro:', error);
-    res.status(500).json({ error: 'Erro ao excluir artigo' });
+    console.error("Erro:", error);
+    res.status(500).json({ error: "Erro ao excluir artigo" });
   }
 });
 
--- src/api/autores.js
+++ src/api/autores.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const client = require('../database/database');
+const client = require("../database/database");
 
 // Rota para pegar autores
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    let query = 'SELECT * FROM facens.autores';
+    let query = "SELECT * FROM facens.autores";
 
     const { id, nome } = req.query;
 
     // Se for fornecido um ID, adicionamos uma cláusula WHERE na consulta
     if (id) {
-      query += ' WHERE id = ? ALLOW FILTERING';
+      query += " WHERE id = ? ALLOW FILTERING";
       const result = await client.execute(query, [id], { prepare: true });
       return res.json(result.rows);
     }
 
     // Se for fornecido um nome, adicionamos uma cláusula WHERE na consulta
     if (nome) {
-      query += ' WHERE nome = ? ALLOW FILTERING';
+      query += " WHERE nome = ? ALLOW FILTERING";
       const result = await client.execute(query, [nome], { prepare: true });
       return res.json(result.rows);
     }
@@ -27,13 +27,43 @@ router.get('/', async (req, res) => {
     const result = await client.execute(query);
     res.json(result.rows);
   } catch (error) {
-    console.error('Erro:', error);
-    res.status(500).json({ error: 'Erro ao buscar autores' });
+    console.error("Erro:", error);
+    res.status(500).json({ error: "Erro ao buscar autores" });
   } finally {
     // Não se esqueça de fechar a conexão após o uso
     await client.shutdown();
   }
 });
 
+// Rota para obter os IDs dos autores dos últimos 10 artigos publicados
+router.get("/ultimos", async (req, res) => {
+  try {
+    // Consulta CQL para selecionar os últimos 10 artigos publicados da materialized view
+    const queryArtigos =
+      "SELECT autor_id FROM facens.artigos_by_data_publicacao LIMIT 10";
+
+    // Executa a consulta para obter os artigos
+    const resultArtigos = await client.execute(queryArtigos);
+
+    if (resultArtigos.rowLength === 0) {
+      return res.json([]);
+    }
+
+    // Extrair autor_id de cada artigo
+    const autorIds = resultArtigos.rows.map((artigo) => artigo.autor_id);
+
+    // Retornar os IDs dos autores como resposta
+    res.json(autorIds);
+  } catch (error) {
+    console.error("Erro:", error);
+    res
+      .status(500)
+      .json({ error: "Erro ao buscar IDs dos autores dos últimos artigos" });
+  }
+});
+
+module.exports = router;
+
+module.exports = router;
 
 module.exports = router;
